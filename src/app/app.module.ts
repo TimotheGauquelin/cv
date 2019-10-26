61c751e6f275bb7b5a7e2dc33d086fc600157d69
Tim <gauquelintimothe@gmail.com>
2019-10-27 00:31:51 +0200

    Creation Core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CoreComponent
   ],
   imports: [
     BrowserModule,
