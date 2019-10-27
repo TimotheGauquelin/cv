d66200a24b08628dc681a4a122d5574f3d21ef21
Tim <gauquelintimothe@gmail.com>
2019-10-27 23:28:20 +0100

    WIP

--- src/app/core/inside-core-projet/inside-core-projet.component.ts
+++ src/app/core/inside-core-projet/inside-core-projet.component.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-inside-core-projet',
   templateUrl: './inside-core-projet.component.html',
   styleUrls: ['./inside-core-projet.component.scss']
 })
-export class InsideCoreProjetComponent implements OnInit {
+export class InsideCoreProjetComponent{
 
   public showPupuceBlock = false;
   public showNutriplusBlock = false;
-  public buttonName = 'Show'
+  public buttonName = 'Show';
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
   displayPupuceBlock() {
     this.showPupuceBlock = !this.showPupuceBlock;
